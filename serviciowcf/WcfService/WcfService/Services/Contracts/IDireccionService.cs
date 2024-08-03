7f197da38763220d61d4cc70e1fcda31dfc997c1
EdChambers <titofarinas@outlook.com>
2024-08-03 10:22:56 -0600

    adding interfaces to Direccion and Prestamo model

---
+++ serviciowcf/WcfService/WcfService/Services/Contracts/IDireccionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfService.Model;
+
+namespace WcfService.Services.Contracts
+{
+    public interface IDireccionService
+    {
+        List<Direccion> GetAll();
+        bool Insert();
+        bool Update();
+        bool Delete();
+    }
+}
