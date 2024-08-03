25696bd581d4cc35db9bd7e6200eb0daf1abdcae
EdChambers <titofarinas@outlook.com>
2024-08-03 11:04:40 -0600

    Delete files to focus on IClienteService, ClienteService

--- serviciowcf/WcfService/WcfService/Cliente.svc.cs
+++ serviciowcf/WcfService/WcfService/Cliente.svc.cs
@@ -1,18 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WcfService.Services.Contracts;
+using System.Configuration;
 
 namespace WcfService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Cliente" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Cliente.svc or Cliente.svc.cs at the Solution Explorer and start debugging.
-    public class Cliente : ICliente
+    public class Cliente 
     {
-        public void DoWork()
+
+        private readonly IClienteService _clienteService;
+               
+
+        public Model.Cliente GetCliente(int clienteId)
         {
+            return _clienteService.GetCliente(clienteId);
+            
+        }
+
+        public int InsertCliente(string numeroIdentificacion, string tipoIdentificacion, string primerNombre, string segundoNombre, string primerApellido, string segundoApellido, int direccionId, bool estado)
+        {
+            return _clienteService.InsertCliente(numeroIdentificacion,tipoIdentificacion,primerNombre,segundoNombre,primerApellido,segundoApellido, direccionId, estado);
+        }
+
+        public void UpdateCliente(int clienteId, string numeroIdentificacion, string tipoIdentificacion, string primerNombre, string segundoNombre, string primerApellido, string segundoApellido, int direccionId, bool estado)
+        {
+            _clienteService.UpdateCliente(clienteId, numeroIdentificacion, tipoIdentificacion, primerNombre, segundoNombre, primerApellido, segundoApellido, direccionId, estado);
+        }
+
+
+        public void DeleteCliente(int clienteId)
+        {
+           _clienteService.DeleteCliente(clienteId);
         }
     }
 }
--- serviciowcf/WcfService/WcfService/Services/Contracts/IClienteService.cs
+++ serviciowcf/WcfService/WcfService/Services/Contracts/IClienteService.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace WcfService.Services.Contracts
 {
     public interface IClienteService
     {
 
-        List<Cliente> GetAll();
-        bool Insert();
-        bool Update();
-        bool Delete();
+        int InsertCliente(string numeroIdentificacion, string tipoIdentificacion, string primerNombre, string segundoNombre, string primerApellido, string segundoApellido, int direccionId, bool estado);
+        Model.Cliente GetCliente(int clienteId);
+        void UpdateCliente(int clienteId, string numeroIdentificacion, string tipoIdentificacion, string primerNombre, string segundoNombre, string primerApellido, string segundoApellido, int direccionId, bool estado);
+        void DeleteCliente(int clienteId);
 
     }
 }
